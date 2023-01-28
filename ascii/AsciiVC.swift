ed4f84c48cb1bc1fc4c90634d9e865dc80143126
Alex <alex.wayne.dev@gmail.com>
2023-01-28 01:58:12 -0700

    Added ability to change quality of image

--- ascii/AsciiVC.swift
+++ ascii/AsciiVC.swift
@@ -11,117 +11,112 @@ import SwiftImage
 
 class AsciiVC: UIViewController {
     
+    let p: ViewController
     let uiImage: UIImage
+    let quality: Float
 
     var asciiText: String = ""
     
-    let asciiLabel: UITextView = {
+    let textView: UITextView = {
         let label = UITextView()
-//        label.backgroundColor = .white
+        label.backgroundColor = UIColor(named: "accent")
 //        label.textColor = .white
+        label.isEditable = false
         label.textAlignment = .center
         label.layer.cornerRadius = 15
-        label.font = .monospacedSystemFont(ofSize: 4, weight: .regular)
-        label.text = """
-               ......                  .............
-            .....;;...                ................
-         .......;;;;;/mmmmmmmmmmmmmm\\/..................
-       ........;;;mmmmmmmmmmmmmmmmmmm.....................
-     .........;;m/;;;;\\mmmmmm/;;;;;\\m......................
-  ..........;;;m;;mmmm;;mmmm;;mmmmm;;m......................
-..........;;;;;mmmnnnmmmmmmmmmmnnnmmmm\\....................
-.........  ;;;;;n/#####\\nmmmmn/#####\\nmm\\.................
-.......     ;;;;n##...##nmmmmn##...##nmmmm\\.............
-....        ;;;n#.....|nmmmmn#.....#nmmmmm,l.........
- ..          mmmn\\.../nmmmmmmn\\.../nmmmm,m,lll.....
-          /mmmmmmmmmmmmmmmmmmmmmmmmmmm,mmmm,llll..
-      /mmmmmmmmmmmmmmmmmmmmmmm\\nmmmn/mmmmmmm,lll/
-   /mmmmm/..........\\mmmmmmmmmmnnmnnmmmmmmmmm,ll
-  mmmmmm|...........|mmmmmmmmmmmmmmmmmmmmmmmm,ll
-  \\mmmmmmm\\......./mmmmmmmmmmmmmmmmmmmmmmmmm,llo
-    \\mmmmmmm\\.../mmmmmmmmmmmmmmmmmmmmmmmmmm,lloo
-      \\mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm,ll/oooo
-         \\mmmmmmmmmmll..;;;.;;;;;;/mmm,lll/oooooo\\
-                   ll..;;;.;;;;;;/llllll/ooooooooo
-                   ll.;;;.;;;;;/.llll/oooooooooo..o
-                   ll;;;.;;;;;;..ll/ooooooooooo...oo
-                   \\;;;;.;;;;;..ll/ooooo...ooooo..oo\\
-                 ;;;;;;;;;;;;..ll|oooo.....oooooooooo
-                ;;;;;;.;;;;;;.ll/oooo.....ooooooo....\\
-                ;;;;;.;;;;;;;ll/ooooooooooooo.....oooo
-                 \\;;;.;;;;;;/oooooooooooo.....oooooooo\\
-                  \\;;;.;;;;/ooooooooo.....ooooooooooooo
-                    \\;;;;/ooooooo.....ooooooooooo...ooo\\
-                    |o\\;/oooo.....ooooooooooooo......ooo
-                    oooooo....ooooooooooooooooooo.....oo\\
-                   oooo....oooooooooooooooooooooooo..oooo
-                  ___.oooooooooooooo....ooooooooooooooooo\\
-                 /XXX\\oooooooooooo.....ooooooooooooooooooo
-                 |XXX|ooooo.oooooo....ooooooooooooooooooooo
-               /oo\\X/oooo..ooooooooooooooooooo..oooooooooooo
-             /ooooooo..ooooo..oooooooooooooo.....ooooooooo...
-           /ooooo...ooooo.....oooooooooooo.......ooooooo.....o
-"""
+        label.font = .monospacedSystemFont(ofSize: 0.5, weight: .regular)
+//        label.isHidden = true
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let nextButton: UIButton = {
+    let closeButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Copy", for: .normal)
         button.setTitleColor(UIColor(named: "bg"), for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
-        button.backgroundColor = .white
+        button.backgroundColor = UIColor(named: "accent")
         button.layer.cornerRadius = 15
+        button.isUserInteractionEnabled = false
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(copyPressed), for: .touchUpInside)
+        button.addTarget(self, action: #selector(closePressed), for: .touchUpInside)
         return button
     }()
     
+    let spinner: UIActivityIndicatorView = {
+        let av = UIActivityIndicatorView(style: .medium)
+        av.color = .white
+        av.hidesWhenStopped = true
+        av.translatesAutoresizingMaskIntoConstraints = false
+        return av
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: "bg")
         
-        view.addSubview(asciiLabel)
-        view.addSubview(nextButton)
+        let fontSize: CGFloat = CGFloat(400 / Float(quality))
+        print(fontSize)
+        textView.font = .monospacedSystemFont(ofSize: fontSize, weight: .regular)
+        
+        view.addSubview(textView)
+        view.addSubview(closeButton)
+        view.addSubview(spinner)
                 
-        nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6).isActive = true
-        nextButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25).isActive = true
+        closeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        closeButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6).isActive = true
+        closeButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        closeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25).isActive = true
 
-        asciiLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        asciiLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        asciiLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
-        asciiLabel.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -25).isActive = true
+        textView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
+        textView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+        textView.bottomAnchor.constraint(equalTo: closeButton.topAnchor, constant: -25).isActive = true
 
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
-        let newSize = resize()
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        spinner.startAnimating()
         
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
-        uiImage.draw(in: rect)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        if let newImage = newImage {
-            let image = Image<RGBA<UInt8>>(uiImage: newImage)
-            let lum = rgbToLuminance(image: image)
-            print(lum.count)
-            for x in 0..<lum.count {
-                for y in 0..<lum[x].count {
-                    asciiText.append(selectAsciiChar(value: lum[x][y]))
-                }
-                asciiText.append("\n")
-            }
+        DispatchQueue.global(qos: .userInitiated).async {
             
-            asciiLabel.text = asciiText
+            let newSize = self.resize()
+            let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+            
+            UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+            self.uiImage.draw(in: rect)
+            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+            var text = ""
+            
+            if let newImage = newImage {
+                let image = Image<RGBA<UInt8>>(uiImage: newImage)
+                let lum = self.rgbToLuminance(image: image)
+                for x in 0..<lum.count {
+                    for y in 0..<lum[x].count {
+                        text.append(self.selectAsciiChar(value: lum[x][y]))
+                    }
+                    text.append("\n")
+                }
+                
+                DispatchQueue.main.async {
+                    self.textView.text = text
+                    self.spinner.stopAnimating()
+                    self.textView.backgroundColor = .white
+                    self.closeButton.backgroundColor = .white
+                    self.closeButton.setTitle("Close", for: .normal)
+                    self.closeButton.isUserInteractionEnabled = true
+                }
+                
+            }
         }
     }
     
-    init(image: UIImage){
+    init(parent: ViewController, image: UIImage, quality: Float){
+        self.p = parent
         self.uiImage = image
+        self.quality = quality
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -132,7 +127,7 @@ class AsciiVC: UIViewController {
     func resize() -> CGSize {
         let ratio = CGFloat(uiImage.size.height) / CGFloat(uiImage.size.width)
         
-        let maxWidth: CGFloat = 100
+        let maxWidth: CGFloat = CGFloat(quality)
         if uiImage.size.width > maxWidth {
             let height = CGFloat(maxWidth) * ratio
             return CGSize(width: maxWidth, height: height)
@@ -169,8 +164,13 @@ class AsciiVC: UIViewController {
         return options[index]
     }
     
-    @objc func copyPressed(){
-        
+    @objc func screenPressed(){
+        print("GO AWAY")
+        textView.resignFirstResponder()
+    }
+    
+    @objc func closePressed(){
+        dismiss(animated: true)
     }
 }
 
--- ascii/ViewController.swift
+++ ascii/ViewController.swift
@@ -9,14 +9,9 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Ascii Art"
-        label.font = .systemFont(ofSize: 42, weight: .bold)
-        label.textColor = .white
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    let defaultQuality: Float = 200
+    let minQuality: Float = 50
+    let maxQuality: Float = 1000
     
     let instructions: UILabel = {
         let label = UILabel()
@@ -53,6 +48,29 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         return button
     }()
     
+    let bottomView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let qualityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Quality"
+        label.textColor = .white
+        label.isHidden = true
+        label.font = .systemFont(ofSize: 14, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let slider: UISlider = {
+        let slider = UISlider()
+        slider.isHidden = true
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        return slider
+    }()
+    
     let nextButton: UIButton = {
         let button = UIButton()
         button.setTitle("Next", for: .normal)
@@ -77,42 +95,59 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         imagePicker.allowsEditing = false
         imagePicker.delegate = self
         
-        view.addSubview(titleLabel)
+        slider.minimumValue = minQuality
+        slider.maximumValue = maxQuality
+        slider.setValue(defaultQuality, animated: false)
+        
         view.addSubview(instructions)
         view.addSubview(imageView)
-        view.addSubview(cameraButton)
-        view.addSubview(galleryButton)
-        view.addSubview(nextButton)
-        print(view.frame.width * 0.9)
-
         
+        view.addSubview(bottomView)
+        
+        bottomView.addSubview(cameraButton)
+        bottomView.addSubview(galleryButton)
+        bottomView.addSubview(qualityLabel)
+        bottomView.addSubview(slider)
+        bottomView.addSubview(nextButton)
+                
         setupSubviews()
     }
 
     func setupSubviews(){
-        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
         
-        instructions.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
+        instructions.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 75).isActive = true
         instructions.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         instructions.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
 
         imageView.topAnchor.constraint(equalTo: instructions.bottomAnchor, constant: 10).isActive = true
-        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+        imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        
+        bottomView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bottomView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
+        bottomView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 5).isActive = true
+        bottomView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15).isActive = true
+                        
+        qualityLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        qualityLabel.bottomAnchor.constraint(equalTo: bottomView.centerYAnchor, constant: -5).isActive = true
+        qualityLabel.widthAnchor.constraint(equalTo: slider.widthAnchor).isActive = true
+        
+        slider.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        slider.topAnchor.constraint(equalTo: qualityLabel.bottomAnchor, constant: 5).isActive = true
+        slider.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6).isActive = true
+        
+        cameraButton.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 5).isActive = true
+        cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -view.frame.width / 6).isActive = true
+        
+        galleryButton.topAnchor.constraint(equalTo: cameraButton.topAnchor).isActive = true
+        galleryButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: view.frame.width / 6).isActive = true
         
         nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.6).isActive = true
         nextButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -25).isActive = true
-        
-        cameraButton.bottomAnchor.constraint(equalTo: nextButton.topAnchor, constant: -25).isActive = true
-        cameraButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -view.frame.width / 6).isActive = true
-        
-        galleryButton.bottomAnchor.constraint(equalTo: cameraButton.bottomAnchor).isActive = true
-        galleryButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: view.frame.width / 6).isActive = true
+        nextButton.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -15).isActive = true
+
     }
     
     @objc func cameraPressed(){
@@ -127,14 +162,17 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     @objc func nextPressed(){
+        print("SDF")
         if let image = imageView.image {
-            present(AsciiVC(image: image), animated: true)
+            present(AsciiVC(parent: self, image: image, quality: slider.value), animated: true)
         }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             self.imageView.image = image
+            qualityLabel.isHidden = false
+            slider.isHidden = false
             nextButton.isHidden = false
         }
         picker.dismiss(animated: true)
